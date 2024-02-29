527618cfccb7af0028eeca66f7b5ff5448ca2be5
sjjunker <sandi.junker@outlook.com>
2024-02-29 16:18:12 -0600

    Add James Bond Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Get first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        //Get last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //Print out James Bond style
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
