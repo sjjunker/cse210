2292b69354a709261f2433a4a58946acc997b456
sjjunker <sandi.junker@outlook.com>
2024-03-01 12:45:48 -0600

    Complete C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Set the Game State
+        string playAgain = "yes";
+
+        while (playAgain == "yes") {
+
+            //Set the magic number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            //User guess
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+            int numberOfGuesses = 1;
+
+            while (userGuess != magicNumber) {
+                //Provide feedback
+                if (userGuess > magicNumber) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                //User guess
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+            }
+
+            //Display Results
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Number of Guesses: {numberOfGuesses}");
+
+            //Ask to play again
+            Console.Write("Would you like to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
