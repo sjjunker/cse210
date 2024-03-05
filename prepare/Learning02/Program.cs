302c8d6923dc10350e0a2cc227110772fe454026
sjjunker <sandi.junker@outlook.com>
2024-03-05 14:30:04 -0600

    Complete Lesson02 Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine();
+
+        //Job One
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2010;
+        job1._endYear = 2017;
+
+        //Job Two
+        Job job2 = new Job();
+        job2._jobTitle = "iOS Mobile Developer";
+        job2._company = "Apple";
+        job2._startYear = 2017;
+        job2._endYear = 2024;
+
+        //New Resume
+        Resume myResume = new Resume();
+        myResume._name = "Billy Jo Bob";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
+
+        Console.WriteLine();
     }
 }
