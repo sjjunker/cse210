cc3656357a10cd62057106568401b58266b084c0
sjjunker <sandi.junker@outlook.com>
2024-03-26 15:54:49 -0500

    Finish W05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //New Shapes
+        Rectangle newRectangle = new Rectangle("blue", 5, 3);
+        Circle newCircle = new Circle("yellow", 7);
+        Square newSquare = new Square("red", 3);
+
+        //List of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(newSquare);
+        shapes.Add(newCircle);
+        shapes.Add(newRectangle);
+
+        //Get the color and area of each
+        foreach(Shape shape in shapes) 
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
